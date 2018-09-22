696511988237a7491fdb0c184d3a121251617728
Onfir3 <w526529650@outlook.com>
2018-09-22 14:24:23 +0800

    一个轮播

---
+++ main.js
@@ -0,0 +1,45 @@
+var allButtons = $('#buttons > span')
+
+for(let i=0 ; i<allButtons.length; i++){
+  $(allButtons[i]).on('click',function(x){
+    var index = $(x.currentTarget).index()
+    var px = index * -600
+    $('#images').css({
+      transform:'translate(' + px + 'px)'
+    })
+    n = index
+    activeButton(allButtons.eq(n))
+  })
+}
+
+
+
+var n = 0;
+var size = allButtons.length;
+playSlide(n%size)
+
+var timerId = setTimer()
+
+function setTimer(){
+  return setInterval(() =>{
+  n += 1
+  playSlide(n%size)
+ },3000)
+}
+
+function playSlide(index){
+  allButtons.eq(index).trigger('click')
+}
+
+function activeButton($button){
+   $button
+  .addClass('red')
+  .siblings('.red').removeClass('red')
+}
+
+$('.window').on('mouseenter',function(){
+  window.clearInterval(timerId)
+})
+$('.window').on('mouseleave',function(){
+  timerId = setTimer()
+})
